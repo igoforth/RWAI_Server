5452ca2bf8c55cfc557928b6b86b17678a1748bd
Ian Goforth <ian.goforth@gmail.com>
2024-05-11 08:43:32 -0400

    Minor changes for reliability.

---
+++ src/AIServer/job_pb2.pyi
@@ -0,0 +1,31 @@
+from google.protobuf import duration_pb2 as _duration_pb2
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class JobRequest(_message.Message):
+    __slots__ = ("job_id", "job_type", "time", "payload")
+    JOB_ID_FIELD_NUMBER: _ClassVar[int]
+    JOB_TYPE_FIELD_NUMBER: _ClassVar[int]
+    TIME_FIELD_NUMBER: _ClassVar[int]
+    PAYLOAD_FIELD_NUMBER: _ClassVar[int]
+    job_id: int
+    job_type: str
+    time: _timestamp_pb2.Timestamp
+    payload: bytes
+    def __init__(self, job_id: _Optional[int] = ..., job_type: _Optional[str] = ..., time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., payload: _Optional[bytes] = ...) -> None: ...
+
+class JobResponse(_message.Message):
+    __slots__ = ("job_id", "job_type", "duration", "payload")
+    JOB_ID_FIELD_NUMBER: _ClassVar[int]
+    JOB_TYPE_FIELD_NUMBER: _ClassVar[int]
+    DURATION_FIELD_NUMBER: _ClassVar[int]
+    PAYLOAD_FIELD_NUMBER: _ClassVar[int]
+    job_id: int
+    job_type: str
+    duration: _duration_pb2.Duration
+    payload: bytes
+    def __init__(self, job_id: _Optional[int] = ..., job_type: _Optional[str] = ..., duration: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., payload: _Optional[bytes] = ...) -> None: ...
