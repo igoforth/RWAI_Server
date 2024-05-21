77b79f9b5dd5afa9bc393659c98cfb934281f372
Ian Goforth <ian.goforth@gmail.com>
2024-05-21 19:50:04 -0400

    Minor tweaks to make logs prettier.

--- src/AIServer/__init__.py
+++ src/AIServer/__init__.py
@@ -10,14 +10,18 @@ import traceback
 from collections import deque
 from time import monotonic
 
-os_name, os_release, os_version = platform.system(), platform.release(), platform.version()
+os_name, os_release, os_version = (
+    platform.system(),
+    platform.release(),
+    platform.version(),
+)
 
 # logging
-logger  = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 # if (logger.hasHandlers()):
 #     logger.handlers.clear()
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 stderr_handler = logging.StreamHandler(sys.stderr)
 stderr_handler.setLevel(logger.getEffectiveLevel())
 stderr_handler.setFormatter(formatter)
@@ -26,7 +30,10 @@ logger.propagate = False
 
 # add to path "FILE/../../../__pypackages__/VERSION_SHORT/lib"
 mjr_mnr = ".".join(platform.python_version_tuple()[:2])
-new_path = str(pathlib.Path(__file__).parent.parent.parent / pathlib.Path(f"__pypackages__/{mjr_mnr}/lib"))
+new_path = str(
+    pathlib.Path(__file__).parent.parent.parent
+    / pathlib.Path(f"__pypackages__/{mjr_mnr}/lib")
+)
 sys.path.append(new_path)
 # import Health
 from . import Client, Server
@@ -35,19 +42,38 @@ MAX_INTERVAL = 30
 RETRY_HISTORY = 3
 
 # ARGS
-LLAMAFILE_FILENAME = "llamafile.com" if os_name == 'Windows' else "llamafile"
+LLAMAFILE_FILENAME = "llamafile.com" if os_name == "Windows" else "llamafile"
 LLAMAFILE_PATH: pathlib.Path = pathlib.Path(os.getcwd()) / "bin" / LLAMAFILE_FILENAME
 LLAMAFILE_PARAMS_LIST: list[str] = [
-    "--host", "127.0.0.1", "--port", "50052",
-    "-ngl", "9999", "--server", "--nobrowser", "-cb",
-    "-m", "models/Phi-3-mini-128k-instruct.Q4_K_M.gguf",
-    "--chat-template", r""" "{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') %}{{'<|user|>' + '\n' + message['content'] + '<|end|>' + '\n' + '<|assistant|>' + '\n'}}{% elif (message['role'] == 'assistant') %}{{message['content'] + '<|end|>' + '\n'}}{% endif %}{% endfor %}" """.strip()
+    "--host",
+    "127.0.0.1",
+    "--port",
+    "50052",
+    "-ngl",
+    "9999",
+    "--server",
+    "--nobrowser",
+    "-cb",
+    "-m",
+    "models/Phi-3-mini-128k-instruct.Q4_K_M.gguf",
+    "--chat-template",
+    r""" "{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') %}{{'<|user|>' + '\n' + message['content'] + '<|end|>' + '\n' + '<|assistant|>' + '\n'}}{% elif (message['role'] == 'assistant') %}{{message['content'] + '<|end|>' + '\n'}}{% endif %}{% endfor %}" """.strip(),
 ]
+
 
 async def run_llama_forever():
     """Capture output (stdout and stderr) while running external command."""
+    stderr_filepath = pathlib.Path("llama_stderr.log")
+    if stderr_filepath.exists():
+        stderr_filepath.unlink()
+    else:
+        stderr_filepath.touch()
+    stderr_file = open(stderr_filepath, "wb")
     proc = await asyncio.create_subprocess_exec(
-        LLAMAFILE_PATH, *LLAMAFILE_PARAMS_LIST, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        LLAMAFILE_PATH,
+        *LLAMAFILE_PARAMS_LIST,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=stderr_file,
     )
 
     try:
@@ -60,34 +86,41 @@ async def run_llama_forever():
             except asyncio.TimeoutError:
                 pass
             else:
-                logger.debug(f'[stdout] {out.decode().strip()}')
-            try:
-                err = await asyncio.wait_for(proc.stderr.read(2048), 0.1)
-            except asyncio.TimeoutError:
-                pass
-            else:
-                logger.debug(f'[stderr] {err.decode().strip()}')
+                logger.debug(f"[AI Server] {out.decode().strip()}")
+            # try:
+            #     err = await asyncio.wait_for(proc.stderr.read(2048), 0.1)
+            # except asyncio.TimeoutError:
+            #     pass
+            # else:
+            #     logger.debug(f"[stderr] {err.decode().strip()}")
 
     except asyncio.CancelledError:
         logger.info("AI gracefully shut down")
     finally:
         await proc.communicate()
+        stderr_file.close()
         # logger.debug(f'{LLAMAFILE_PATH} {" ".join(LLAMAFILE_PARAMS_LIST)} exited with {proc.returncode}')
+
 
 def supervise(func, name=None, retry_history=RETRY_HISTORY, max_interval=MAX_INTERVAL):
     """Simple wrapper function that automatically tries to name tasks"""
     if name is None:
-        if hasattr(func, '__name__'): # raw func
+        if hasattr(func, "__name__"):  # raw func
             name = func.__name__
-        elif hasattr(func, 'func'): # partial
+        elif hasattr(func, "func"):  # partial
             name = func.func.__name__
-    return asyncio.create_task(supervisor(func, name, retry_history, max_interval), name=name)
+    return asyncio.create_task(
+        supervisor(func, name, retry_history, max_interval), name=name
+    )
 
-async def supervisor(func, name, retry_history=RETRY_HISTORY, max_interval=MAX_INTERVAL):
+
+async def supervisor(
+    func, name, retry_history=RETRY_HISTORY, max_interval=MAX_INTERVAL
+):
     """Takes a noargs function that creates a coroutine, and repeatedly tries
-        to run it. It stops is if it thinks the coroutine is failing too often or
-        too fast.  """
-    start_times = deque([float('-inf')], maxlen=retry_history)
+    to run it. It stops is if it thinks the coroutine is failing too often or
+    too fast."""
+    start_times = deque([float("-inf")], maxlen=retry_history)
     while True:
         start_times.append(monotonic())
         try:
@@ -95,8 +128,8 @@ async def supervisor(func, name, retry_history=RETRY_HISTORY, max_interval=MAX_I
         except Exception:
             if min(start_times) > monotonic() - max_interval:
                 logger.critical(
-                    f'Failure in task {asyncio.current_task().get_name()!r}.'
-                    ' Is it in a restart loop?'
+                    f"Failure in task {asyncio.current_task().get_name()!r}."
+                    " Is it in a restart loop?"
                 )
                 # we tried our best, this coroutine really isn't working.
                 # We should try to shutdown gracefully by setting a global flag
@@ -104,29 +137,33 @@ async def supervisor(func, name, retry_history=RETRY_HISTORY, max_interval=MAX_I
                 # see that it is set. However, here we just reraise the exception.
                 raise
             else:
-                logger.error(name, 'failed, will retry. Failed because:')
+                logger.error(name, "failed, will retry. Failed because:")
                 traceback.print_exc()
+
 
 class GracefulExit(SystemExit):
     code = 1
 
+
 def raise_graceful_exit(*args):
     raise GracefulExit()
+
 
 async def main_async(iq, oq):
     tasks = [
         supervise(run_llama_forever),
         supervise(functools.partial(Server.run, iq, oq)),
-        supervise(functools.partial(Client.run, iq, oq))
+        supervise(functools.partial(Client.run, iq, oq)),
     ]
     await asyncio.wait(
         tasks,
         # Only stop when all coroutines have completed
         # -- this allows for a graceful shutdown
-        # Alternatively use FIRST_EXCEPTION to stop immediately 
+        # Alternatively use FIRST_EXCEPTION to stop immediately
         return_when=asyncio.ALL_COMPLETED,
     )
     return tasks
+
 
 def main():
     iq = asyncio.Queue()
@@ -135,14 +172,14 @@ def main():
     # health_watcher = Health.HealthWatcher()
 
     loop = asyncio.get_event_loop()
-    signal.signal(signal.SIGINT,  raise_graceful_exit)
+    signal.signal(signal.SIGINT, raise_graceful_exit)
     signal.signal(signal.SIGTERM, raise_graceful_exit)
 
     try:
         tasks = asyncio.ensure_future(main_async(iq, oq), loop=loop)
         loop.run_until_complete(tasks)
     except GracefulExit:
-        logger.info('Got signal: SIGINT, shutting down.')
+        logger.info("Got signal: SIGINT, shutting down.")
     finally:
         tasks = asyncio.all_tasks(loop=loop)
         for t in tasks:
